4608abb75ec6f1ec1b4eded05f3644241c710846
alex h <alextheastro@gmail.com>
2025-09-06 04:02:18 -0700

    update link order for better hierarchy

--- components/Links.tsx
+++ components/Links.tsx
@@ -9,9 +9,10 @@ const spaceMono = Space_Mono({
 export default function Links() {    
     return (
         <div className={`absolute flex top-2 gap-1 left-2 flex-col text-white text-sm ${spaceMono.className}`}>
+                        <a href="https://www.instagram.com/sdxucsd/" target="_blank" rel="noopener noreferrer" className="hover:underline">instagram</a>
+                        <a href="https://www.linkedin.com/company/104974111" target="_blank" rel="noopener noreferrer" className="hover:underline">linkedin</a>
+
             <a href="https://discord.gg/GTR9Q3XR" target="_blank" rel="noopener noreferrer" className="hover:underline">discord</a>
-            <a href="https://www.linkedin.com/company/104974111" target="_blank" rel="noopener noreferrer" className="hover:underline">linkedin</a>
-            <a href="https://www.instagram.com/sdxucsd/" target="_blank" rel="noopener noreferrer" className="hover:underline">instagram</a>
             <a href="https://github.com/SDxUCSD" target="_blank" rel="noopener noreferrer" className="hover:underline">github</a>
         </div>
     );
